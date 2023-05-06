a1e38826197f07907848f50871558708d11c3c3a
mj <akbarian.dev@gmail.ccom>
2023-05-06 15:54:55 +0330

    feat: SWAP-41 add network (chain) type field

--- entity/chain.go
+++ entity/chain.go
@@ -1,11 +1,19 @@
 package entity
 
+type ChainType string
+
+const (
+	MainChainType ChainType = "main"
+	TestChainType           = "test"
+)
+
 type Chain struct {
-	Id       string   `yaml:"id"`
-	ParentId string   `yaml:"parent_id"`
-	Name     string   `yaml:"name"`
-	Symbol   string   `yaml:"symbol"`
-	LogoPNG  string   `yaml:"logo_png"`
-	ChainId  string   `yaml:"chain_id"`
-	Assets   []*Asset `yaml:"assets"`
+	Id       string    `yaml:"id"`
+	ParentId string    `yaml:"parent_id"`
+	Name     string    `yaml:"name"`
+	Symbol   string    `yaml:"symbol"`
+	LogoPNG  string    `yaml:"logo_png"`
+	ChainId  string    `yaml:"chain_id"`
+	Assets   []*Asset  `yaml:"assets"`
+	Type     ChainType `yaml:"type"`
 }
